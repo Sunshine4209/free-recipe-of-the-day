decb053b26876f8caf66ef6b447b9f77b355b90e
vero <vero@busybusy.com>
2017-07-14 17:37:14 +0200

    removed upload folder

--- wp-content/menuplanner/menuplanner.php
+++ wp-content/menuplanner/menuplanner.php
@@ -1,10 +1,197 @@
-<div class="menu-planner">
-
-    <div class="recipes-of-week">
-        <h1>This week's meal plan</h1>
-
-		coming soon....
-    </div><!-- ./recipes-of-week -->
-
-    <div style="clear: both;"></div>
+<div>
+    <h1>This Week Menu Plan</h1>
 </div>
+
+<div style="height: 500px;">
+
+		<div id="mp_mainContainer">
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_1"><img src="wp-content/uploads/2012/06/beansenchiladas-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=593" rel="bookmark" title="Permanent Link: Beans & Cheese Burritos">Beans and Cheese Burritos</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=593#respond" title="Comment on Beans & Cheese Burritos">0 Comments</a></div></div>
+			</div>
+
+			</div>	
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_2"><img src="wp-content/uploads/2012/06/salsaVerde-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Salsa Verde</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+
+			</div>	
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_3"><img src="wp-content/uploads/2012/08/buffalochickenwrap-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Buffalo chicken wrap</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+		</div>	
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_4"><img src="wp-content/uploads/2012/08/tomatoprovencale-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Tomatoes à la provençale</a></h5>
+
+			<div>
+			<div class="mp_excerpt">This homemade soup is delicious and filling. The spices, fresh herbs, and ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_5"><img src="wp-content/uploads/2013/01/fiveSpiceTurkeyWrap-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Chinese Five Spice Turkey Wrap</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+
+		</div>
+	
+	</div> 
+
+	<div id="mp_grocerylist" class="note-wrapper">
+		<div class="mp_note-top"></div>
+		<div class="note-content">
+		<ul id="mp_mygrocerylist">
+			<li>Spaghetti sauce</li>
+			<li>Refried beans</li>
+			<li>Tomato sauce</li>
+			<li>Red Peppers (2)</li>
+			<li>Buffalo chicken</li>
+			<li>Blue cheese dressing</li>
+			<li>cole slaw</li>
+			<li>Flour tortillas</li>
+			<li>Tomatoes</li>
+			<li>Ground turkey</li>
+			<li>Cheddar cheese</li>
+			<li>Bibb lettuce</li>
+			<li>Sliced water chesnuts</li>
+			<li>Ginger</li>
+		</ul>
+		</div>
+		<div class="note-bottom"></div>
+	</div>
+
+
+</div>
+
+
+<hr class="mp_hr" />
+<div id="mp_customGrocery">
+    <div><h1>Or create your own Menu Plan</h1></div>
+    <p>create your own menu plan by entering up to 6 ingredients in the boxes below. </p>
+<div class="mp_girlsearch"><img src="wp-content/menuplanner/girlmenu.png" width="100" height="150" style="border:none;" /></div>
+<form enctype="multipart/form-data" id="upload_ingredients">
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 1" name="i1" onfocus="if (this.value == 'Ingredient 1') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 1'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 2" name="i2" onfocus="if (this.value == 'Ingredient 2') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 2'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 3" name="i3" onfocus="if (this.value == 'Ingredient 3') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 3'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 4" name="i4" onfocus="if (this.value == 'Ingredient 4') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 4'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 5" name="i5" onfocus="if (this.value == 'Ingredient 5') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 5'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="text" value="Ingredient 6" name="i6" onfocus="if (this.value == 'Ingredient 6') {this.value = '';}" onblur="if (this.value == '') { this.value = 'Ingredient 6'; }">
+	</div>
+	<div class="mp_searchingredient">
+	<input type="submit" value="Create Menu Plan" id="submit-ingredients" onclick="return submit_form()">
+	</div>
+</form>
+</div>
+
+
+
+<!--<div id="pickRecipes">-->
+<!--	<div id="main-dialog">-->
+<!--		<div id="post-dialog">-->
+<!--			<div class="post-content-dialog">-->
+<!--				<div><h4>Select 5 recipes to add to your custom menu.</h4></div>-->
+<!--				<div id="mp_mainContainer-dialog">-->
+<!--					-->
+<!--					-->
+<!--					-->
+<!--					-->
+<!--				</div>-->
+<!---->
+<!---->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--</div>-->
+
+<!--<div id="myGroceryListDialog">-->
+<!--	<div class="grocery_dialog_intro">Select the ingredients you already have to delete them from the list.<br /></div>-->
+<!--	<form><div class="mp_searchingredient"><input type="text" id="grocerysend" value="Enter Email" name="email"></div></form>-->
+<!--	<div class="note-wrapper">-->
+<!--		<div class="mp_note-top"></div>-->
+<!--		<div class="note-content">-->
+<!--			<ul id="mp_mygrocerylist-dialog">-->
+<!---->
+<!---->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<div class="note-bottom"></div>-->
+<!--	</div>-->
+<!--	-->
+<!--</div>-->
+
+<div class="a-recipe">
+    <h6>Monday</h6>
+    <div class="recipe-picture" id="monday-recipe"><img src="wp-content/uploads/2012/06/beansenchiladas-80x70.png" width="80" height="70" /></div>
+    <div class="recipe-title">
+
+        <h5><a href="http://www.freerecipeoftheday.com/?p=593" rel="bookmark" title="Permanent Link: Beans & Cheese Burritos">Beans and Cheese Burritos</a></h5>
+
+        <div>
+            <div class="excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+        </div>
+
+    </div>
+</div><!-- ./a-recipe -->
+
+<div class="a-recipe">
+    <div class="recipe-picture" id="mp_pic_2"><img src="wp-content/uploads/2012/06/beansenchiladas-80x70.png" width="80" height="70" /></div>
+    <div class="recipe-title">
+        <h5><a href="http://www.freerecipeoftheday.com/?p=593" rel="bookmark" title="Permanent Link: Beans & Cheese Burritos">Beans and Cheese Burritos</a></h5>
+
+        <div>
+            <div class="excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+        </div>
+    </div>
+</div><!-- ./a-recipe -->
---
+++ wp-content/menuplanner/menuplannerBis.php
@@ -0,0 +1,10 @@
+<div class="menu-planner">
+
+    <div class="recipes-of-week">
+        <h1>This week's meal plan</h1>
+
+		coming soon....
+    </div><!-- ./recipes-of-week -->
+
+    <div style="clear: both;"></div>
+</div>
