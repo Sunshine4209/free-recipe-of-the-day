ab165cb22f0c24a6d3d42d21fdf08b876662c517
vero <vero@busybusy.com>
2017-10-12 18:50:23 -0600

    clean up

---
+++ wp-content/menuplanner/menuplannerInProgressBis.php
@@ -0,0 +1,101 @@
+<div>
+    <h1>Menu Plan I</h1>
+</div>
+
+<div style="height: 500px;">
+
+		<div id="mp_mainContainer">
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_1"><img src="wp-content/uploads/2012/06/beansenchiladas-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=593" rel="bookmark" title="Permanent Link: Beans & Cheese Burritos">Beans and Cheese Burritos</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			</div>
+
+			</div>
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_2"><img src="wp-content/uploads/2012/06/salsaVerde-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Salsa Verde</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+
+			</div>	
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_3"><img src="wp-content/uploads/2012/08/buffalochickenwrap-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Buffalo chicken wrap</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+		</div>	
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_4"><img src="wp-content/uploads/2012/08/tomatoprovencale-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Tomatoes à la provençale</a></h5>
+
+			<div>
+			<div class="mp_excerpt">This homemade soup is delicious and filling. The spices, fresh herbs, and ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+
+		</div>
+
+		<div class="mywrapper">
+			<div class="mp_picture" id="mp_pic_5"><img src="wp-content/uploads/2013/01/fiveSpiceTurkeyWrap-80x70.png" width="80" height="70" /></div>
+			<div class="mp_recipename">
+			<h5><a href="http://www.freerecipeoftheday.com/?p=914" rel="bookmark" title="Permanent Link: Chinese Five Spice Turkey Wrap">Chinese Five Spice Turkey Wrap</a></h5>
+
+			<div>
+			<div class="mp_excerpt">Healthy and Delicious. Not only those wraps are mouth watering but they are ...</div>
+			<div class="post-item-meta">
+			<div class="comments"><a href="http://www.freerecipeoftheday.com/?p=541#respond" title="Comment on Bacon, Leeks, and Goat Cheese Quiche">0 Comments</a></div></div>
+			</div>
+			</div>	
+
+		</div>
+	
+	</div> 
+
+	<div id="mp_grocerylist" class="note-wrapper">
+		<div class="mp_note-top"></div>
+		<div class="note-content">
+		<ul id="mp_mygrocerylist">
+			<li>Spaghetti sauce</li>
+			<li>Refried beans</li>
+			<li>Tomato sauce</li>
+			<li>Red Peppers (2)</li>
+			<li>Buffalo chicken</li>
+			<li>Blue cheese dressing</li>
+			<li>cole slaw</li>
+			<li>Flour tortillas</li>
+			<li>Tomatoes</li>
+			<li>Ground turkey</li>
+			<li>Cheddar cheese</li>
+			<li>Bibb lettuce</li>
+			<li>Sliced water chesnuts</li>
+			<li>Ginger</li>
+		</ul>
+		</div>
+		<div class="note-bottom"></div>
+	</div>
+</div>
+
